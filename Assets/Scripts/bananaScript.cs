debb6625c67feaf1592be6d75cd90e8b7df4303e
tim88435 <timofey.kond@gmail.com>
2022-02-25 12:02:07 +1100

    Homework

--- Assets/Scripts/bananaScript.cs
+++ Assets/Scripts/bananaScript.cs
@@ -5,22 +5,58 @@ using UnityEngine;
 public class bananaScript : MonoBehaviour
 {
     public Transform player;
-    public float Speed = 1.0f;
-    public GameObject position0;
-    public float minGoalDIstance = 0.01f;
-    public float chaseDistance = 5f;
+    public float Speed = 1f;
+    public float minGoalDistance = 0.01f;
+    public float chaseDistance = 5;
+    public Transform[] waypoints;
+    public int waypointIndex;
+    public bool isChasing = false;
+
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.position)<chaseDistance)
-            {
+        if (Vector2.Distance(transform.position, player.position) < chaseDistance)
+        {
+            isChasing = true;
             AIMovementTowards(player);
+        }
+        else
+        {
+            if (isChasing == true)
+            {
+                isChasing = false;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (Vector2.Distance(transform.position, waypoints[i].position) < Vector2.Distance(transform.position, waypoints[waypointIndex].position))
+                    {
+                        waypointIndex = i;
+                    }
+                }
             }
+            else
+            {
+                WaypointUpdate();
+            }
+            
+            AIMovementTowards(waypoints[waypointIndex]);
+        }
         //Vector3.MoveTowards(transform.position, position0.transform.position, Time.deltaTime * Speed);
+    }
+    private void WaypointUpdate()
+    {
+        Vector2 AIPosition = transform.position;
+        if (Vector2.Distance(AIPosition, waypoints[waypointIndex].position) < minGoalDistance)
+        {
+            waypointIndex++;
+            if (waypointIndex >= waypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+        }
     }
     private void AIMovementTowards(Transform goal)
     {
-        if (Vector2.Distance(transform.position, goal.position) > minGoalDIstance)
+        if (Vector2.Distance(transform.position, goal.position) > minGoalDistance)
         {
             Vector2 directionToPos0 = goal.position-transform.position;
                     directionToPos0.Normalize();
